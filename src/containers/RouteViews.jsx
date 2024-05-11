fdb256221310dd95f0a20e20331b33bd89a9d246
Pushpendra Pal <p_pal@bt.iitr.ac.in>
2024-05-12 00:55:32 +0530

    routing bug fixes

--- src/containers/RouteViews.jsx
+++ src/containers/RouteViews.jsx
@@ -1,11 +1,13 @@
 import {Routes,Route} from "react-router-dom";
 import HomePage from "../pages/HomePage";
+import My404Page from "../pages/My404Page";
 
 const RouteViews = () => {
     return (
       <main>
         <Routes>
-              <Route exact path="/" element={<HomePage/>} />
+              <Route exact path="/portfolio" element={<HomePage/>} />
+              <Route path='*' exact={true} element={<My404Page/>} />
           </Routes>
       </main>
     )
