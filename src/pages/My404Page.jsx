0b5237f9a243a3ad98c4807e966714af524167e6
Pushpendra Pal <94526347+Pushpendra100@users.noreply.github.com>
2024-09-07 18:01:41 +0530

    404page udpated

--- src/pages/My404Page.jsx
+++ src/pages/My404Page.jsx
@@ -1,7 +1,9 @@
 const My404Page = () => {
   return (
-    <div className='nothingComponent'>Nothing here <br /> 404...</div>
-  )
-}
+    <div className="flex w-[100%] h-[100vh] justify-center items-center text-center text-[2rem] ">
+      Nothing here <br /> 404...
+    </div>
+  );
+};
 
 export default My404Page;
